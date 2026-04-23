68cf683f212c68d678df8fe376b6ce2b2993e0ff
jarrysix <jarrysix@gmail.com>
2026-04-23 12:32:23 +0800

    feat: zap日志打印增加彩色显示

--- common/log/log.go
+++ common/log/log.go
@@ -2,8 +2,11 @@ package log
 
 import (
 	"fmt"
-	"go.uber.org/zap"
 	"openscrm/app/constants"
+	"os"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 var Sugar *zap.SugaredLogger
@@ -12,9 +15,33 @@ var Env string
 
 func SetupLogger(env string) {
 	Env = env
-	Logger, _ = zap.NewDevelopment()
+	var err error
+
+	// 开发环境使用彩色日志
 	if env == constants.PROD {
-		Logger, _ = zap.NewProduction()
+		Logger, err = zap.NewProduction()
+	} else {
+		// 开发环境配置彩色日志
+		encoderConfig := zapcore.EncoderConfig{
+			TimeKey:        "time",
+			LevelKey:       "level",
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			FunctionKey:    zapcore.OmitKey,
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			LineEnding:     zapcore.DefaultLineEnding,
+			EncodeLevel:    zapcore.CapitalColorLevelEncoder, // 彩色级别
+			EncodeTime:     zapcore.ISO8601TimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		}
+		encoder := zapcore.NewConsoleEncoder(encoderConfig)
+		Logger = zap.New(zapcore.NewCore(encoder, zapcore.AddSync(os.Stdout), zapcore.DebugLevel))
+	}
+
+	if err != nil {
+		panic(err)
 	}
 	defer Logger.Sync() // flushes buffer, if any
 	Sugar = Logger.Sugar()
@@ -29,4 +56,24 @@ func TracedError(msg string, err error) {
 		fmt.Printf("%s %+v", msg, err)
 		return
 	}
+}
+
+// Error 打印错误日志，支持彩色显示
+func Error(args ...interface{}) {
+	Sugar.Error(args...)
+}
+
+// Errorf 打印格式化错误日志，支持彩色显示
+func Errorf(template string, args ...interface{}) {
+	Sugar.Errorf(template, args...)
+}
+
+// Info 打印信息日志，支持彩色显示
+func Info(args ...interface{}) {
+	Sugar.Info(args...)
+}
+
+// Infof 打印格式化信息日志，支持彩色显示
+func Infof(template string, args ...interface{}) {
+	Sugar.Infof(template, args...)
 }
